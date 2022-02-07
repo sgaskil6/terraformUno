ba53a112b3712504b9fb4c3b84b0cd094aba82dd
sgaskil6 <helpinmyself2oo@gmail.com>
2022-02-07 00:57:29 -0500

    Rebuilt and committed

---
+++ terraform/jenkins/main.tf
@@ -0,0 +1,17 @@
+provider "aws" {
+	region = "us-east-1"
+}
+
+variable "name" {
+	description = "Name the instance on deploy"
+}
+
+resource "aws_instance" "steph07_jenkins" {
+	ami = "ami-04505e74c0741db8d"
+	instance_type = "t2.micro"
+	key_name = "newProjects"
+
+	tags = {
+		Name = "${var.name}"
+	}
+}
